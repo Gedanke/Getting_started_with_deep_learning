444caa92d42750db3c85d6323f15095b74bfea04
e <13767927306@163.com>
2020-02-12 22:02:19 +0800

    提交

---
+++ Chapter3/ch03/test.py
@@ -0,0 +1,16 @@
+import sys
+import os
+from mnist import load_mnist
+
+# 第一次调用会花费几分钟 ……
+(x_train, t_train), (x_test, t_test) = load_mnist(flatten=True,
+                                                  normalize=False)
+# 输出各个数据的形状
+print(x_train.shape)
+# (60000, 784)
+print(t_train.shape)
+# (60000,)
+print(x_test.shape)
+# (10000, 784)
+print(t_test.shape)
+# (10000,)
